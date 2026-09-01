fdb7fcaad61d35d80661dff68557a397a766763f
persifall1999 <persifall1363@gmail.com>
2026-09-01 05:35:05 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/4.2.3-Site-vacancy/',
   plugins: [react()],
   test: {
     globals: true,
