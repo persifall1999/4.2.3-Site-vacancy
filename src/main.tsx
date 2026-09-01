da7ff274f862f354040a3f07d290425ba6ef53bd
Dmitriy Volkov <persifall1363@gmail.com>
2026-09-01 04:59:11 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
